ff783e2f2c9ed89e50ee9370b94badf5dd4d950c
Forest828 <89926378+Forest828@users.noreply.github.com>
2024-03-24 18:30:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+
+import pygame
+import pygame_constroller_support
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((300, 300))
+Controller = pygame_constroller_support.Controller()
+controllers = []
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("monospace", 15)
+
+pygame.display.set_caption("controller test")
+
+x = 300
+y = 300
+
+def label(text: str, x: int, y: int):
+    screen.blit(font.render(text, 1, (255, 255, 255)), (x, y))
+
+
+run = True
+while run:
+    clock.tick(60)
+    screen.fill(pygame.Color(51, 51, 51))
+
+
+    for controller in controllers:
+        j = Controller.joysticks(controller)
+        t = Controller.triggers(controller)
+        b = Controller.buttons(controller)
+        d = Controller.d_pad(controller)
+        label(f"L_HOR: {j['L_HOR']}", 10, 10)
+        label(f"L_VERT: {j['L_VERT']}", 10, 25)
+        label(f"B_HOR: {j['R_HOR']}", 10, 40)
+        label(f"B_VERT: {j['R_VERT']}", 10, 55)
+        label(f"RT: {t['RT']}", 10, 70)
+        label(f"LT: {t['LT']}", 10, 85)
+        label(f"A: {b['A']}", 10, 100)
+        label(f"B: {b['B']}", 10, 115)
+        label(f"X: {b['X']}", 10, 130)
+        label(f"Y: {b['Y']}", 10, 145)
+        label(f"LB: {b['LB']}", 10, 160)
+        label(f"RB: {b['RB']}", 10, 175)
+        label(f"BACK: {b['BACK']}", 10, 190)
+        label(f"START: {b['START']}", 10, 205)
+        label(f"HOME: {b['HOME']}", 10, 220)
+        label(f"L_STICK_IN: {b['L_STICK_IN']}", 10, 235)
+        label(f"R_STICK_IN: {b['R_STICK_IN']}", 10, 250)
+        label(f"D_HOR: {d['hor']}", 10, 265)
+        label(f"D_VERT: {d['vert']}", 10, 280)
+
+                
+
+    for e in pygame.event.get():
+        if e.type == pygame.JOYDEVICEADDED:
+            controllers.append(pygame.joystick.Joystick(e.device_index))
+        if e.type == pygame.QUIT:
+            run = False
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                run = False
+    
+    pygame.display.flip()
+
+pygame.quit()
--- pygame_constroller_support.py
+++ pygame_constroller_support.py
@@ -1,44 +1,87 @@
 
 import pygame
+import sys
+
+LINUX = 0
+WINDOWS = 1
+
+if sys.platform == "linux":
+    platform = LINUX
+elif sys.platform == "win32":
+    platform = WINDOWS
+
+if platform == LINUX:
+    L_JOYSCTICK_HOR = 0
+    L_JOYSCTICK_VERT = 1
+    R_JOYSCTICK_HOR = 3
+    R_JOYSCTICK_VERT = 4
+    LT = 5
+    RT = 2
+    A = 0
+    B = 1
+    X = 2
+    Y = 3
+    LB = 4
+    RB = 5
+    BACK = 6
+    START = 7
+    HOME = 8
+    L_STICK_IN = 9
+    R_STICK_IN = 10
+elif platform == WINDOWS:
+    L_JOYSCTICK_HOR = 0
+    L_JOYSCTICK_VERT = 1
+    R_JOYSCTICK_HOR = 0
+    R_JOYSCTICK_VERT = 0
+    LT = 5
+    RT = 2
+    A = 0
+    B = 1
+    X = 2
+    Y = 3
+    LB = 4
+    RB = 5
+    BACK = 6
+    START = 7
+    HOME = 8
+    L_STICK_IN = 9
+    R_STICK_IN = 10
+
 
 class Controller():
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.joystick.init()
         
-    def left_joystick(self, controller : pygame.joystick.JoystickType) -> dict:
-        return  {
-            "hor": controller.get_axis(0),
-            "vert": controller.get_axis(1)
+    def joysticks(self, controller: pygame.joystick.JoystickType) -> dict:
+        return {
+            "L_HOR": controller.get_axis(L_JOYSCTICK_HOR),
+            "L_VERT": controller.get_axis(L_JOYSCTICK_VERT),
+            "R_HOR": controller.get_axis(R_JOYSCTICK_HOR),
+            "R_VERT": controller.get_axis(R_JOYSCTICK_VERT)
         }
     
-    def right_joystick(self, controller : pygame.joystick.JoystickType) -> dict:
-        return  {
-            "hor": controller.get_axis(3),
-            "vert": controller.get_axis(4)
+    def triggers(self, controller: pygame.joystick.JoystickType) -> dict:
+        return {
+            "RT": controller.get_axis(RT),
+            "LT": controller.get_axis(LT)
         }
 
-    def triggers(self, controller : pygame.joystick.JoystickType) -> dict:
-        return  {
-            "RT": controller.get_axis(2),
-            "LT": controller.get_axis(5)
+    def buttons(self, controller: pygame.joystick.JoystickType) -> dict:
+        return {
+            "A": controller.get_button(A),
+            "B": controller.get_button(B),
+            "X": controller.get_button(X),
+            "Y": controller.get_button(Y),
+            "LB": controller.get_button(LB),
+            "RB": controller.get_button(RB),
+            "BACK": controller.get_button(BACK),
+            "START": controller.get_button(START),
+            "HOME": controller.get_button(HOME),
+            "L_STICK_IN": controller.get_button(L_STICK_IN),
+            "R_STICK_IN": controller.get_button(R_STICK_IN)
         }
 
-    def buttons(self, controller : pygame.joystick.JoystickType) -> dict:
-        return  {
-            "A": controller.get_button(0),
-            "B": controller.get_button(1),
-            "X": controller.get_button(2),
-            "Y": controller.get_button(3),
-            "LB": controller.get_button(4),
-            "RB": controller.get_button(5),
-            "BACK": controller.get_button(6),
-            "START": controller.get_button(7),
-            "HOME": controller.get_button(8),
-            "L_STICK_IN": controller.get_button(9),
-            "R_STICK_IN": controller.get_button(10)
-        }
-
-    def d_pad(self, controller : pygame.joystick.JoystickType) -> dict:
+    def d_pad(self, controller: pygame.joystick.JoystickType) -> dict:
         hat_x, hat_y = controller.get_hat(0)
         return  {
             "hor": hat_y,
